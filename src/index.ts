8b8623f3a13e9b0cf500e5d87927f006a83d2a1f
YakovLya <yakaulyahovich@gmail.com>
2023-07-03 00:50:36 +0300

    init: start project

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import * as http from 'http';
+import "dotenv/config";
+
+const server = http.createServer();
+
+server.on('request', async (request, response) => {
+  console.log('SAAA');
+});
+
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
